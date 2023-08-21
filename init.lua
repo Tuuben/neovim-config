e0ef332703c5ffa9be17f5fb74863abe0067297b
TobiasH <tobbe.helsing@gmai.com>
2023-08-21 19:07:00 +0300

    chore: config

--- init.lua
+++ init.lua
@@ -3,6 +3,12 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- relative line numbers
+vim.cmd("set relativenumber");
+
+-- Remove background
+vim.cmd('highlight Normal guibg=none')
+
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
@@ -171,7 +177,8 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  { import = 'custom.plugins.nvim-tree' }
+  { import = 'custom.plugins.nvim-tree' },
+  { import = 'custom.plugins.nvim-web-devicons' }
 }, {})
 
 -- [[ Setting options ]]
