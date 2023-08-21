e42dee12571fac6c931ba1de08598ebfc1325d5f
TobiasH <tobbe.helsing@gmai.com>
2023-08-21 19:48:15 +0300

    chore: nvim tree hotkey update

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -3,8 +3,11 @@ return {
 	config = function()
 		local nvimTree = require("nvim-tree");
 
-		vim.keymap.set('n', '<leader>v', function() vim.cmd("NvimTreeToggle") end ,{ desc = 'Toggle file tree' })
-		vim.keymap.set('n', '<leader>vf', function() vim.cmd("NvimTreeFindFile") end,{ desc = 'Find file in File Tree' })
+		vim.keymap.set('n', '<leader>v', function() vim.cmd("NvimTreeToggle") end, { desc = 'Toggle file tree' })
+		vim.keymap.set('n', '<leader>vs', function() vim.cmd("NvimTreeFindFile") end,
+			{ desc = 'Search file in File Tree' })
+		vim.keymap.set('n', '<leader>vf', function() vim.cmd("NvimTreeFindFile") end,
+			{ desc = 'Find file in File Tree' })
 
 		nvimTree.setup()
 	end
